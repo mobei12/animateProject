c063ddb3a8caf2df0c9ccab73743dfcc78c68c40
mobei12 <505453856@qq.com>
2019-06-14 15:22:45 +0800

    添加SVG图标变化动画

---
+++ src/svgDemo/index.js
@@ -0,0 +1,17 @@
+import $ from "jquery";
+import SVGMorpheus from "../../static/js/svg-morpheus"
+var eleGs = document.querySelectorAll("g");
+var myIcons = new SVGMorpheus("#icon");
+
+var index = 0;
+var funMorpheus = function() {
+  myIcons.to(eleGs[index].id);
+  index++;
+  if (!eleGs[index]) {
+    index = 0;
+    document.body.style.fill = "#" + (Math.random() + "").slice(-6);
+  }
+
+  setTimeout(funMorpheus, 2000);
+};
+funMorpheus();
