98b1fd4bb085a91e91f05c23b7df5698af7e069d
mobei12 <505453856@qq.com>
2019-06-18 09:31:08 +0800

    添加新的依赖文件

--- src/lottieDemo/index.js
+++ src/lottieDemo/index.js
@@ -1,10 +1,11 @@
-import bodymovin from "../../static/js/bodymovin"
+import lottie from "../../static/js/lottie"
 const animationData = require('../../static/lotties/data.json');
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById("main"),
-    renderer: "svg",
-    loop: true,
-    autoplay: true,
-    path: 'https://assets2.lottiefiles.com/packages/lf20_FyqMgR.json',//文件路径
-    //animationData:animationData //文件数据
-  });
+
+lottie.loadAnimation({
+  container: document.getElementById("main"), // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  //path: 'https://assets2.lottiefiles.com/packages/lf20_FyqMgR.json' // the path to the animation json
+  animationData:animationData //文件数据
+});
