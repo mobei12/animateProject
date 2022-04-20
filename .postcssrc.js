1401819b083aa68bc5104a1d045e58749a1157ee
蒋军晖 <mobeigege@icloud.com>
2022-04-20 10:41:51 +0800

    bug fix

--- .postcssrc.js
+++ .postcssrc.js
@@ -2,10 +2,8 @@
 
 module.exports = {
   // "parser": "postcss-strip-inline-comments",
-  "plugins": {
+  plugins: {
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {
-      browsers: ["last 10 versions","Android >= 4.0"]
-    },
-  }
+    autoprefixer: { overrideBrowserslist: ['> 0.15% in CN'] },
+  },
 }
