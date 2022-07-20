4364c373b61a6e8e898716f88a2c1345ac51ebb6
漠北 <j505453856@gmail.com>
2022-07-20 17:22:24 +0800

    bug fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,135 +1,151 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
-    </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
-  </v-app>
+	<v-app dark>
+		<v-navigation-drawer
+			v-model="drawer"
+			:mini-variant="miniVariant"
+			:clipped="clipped"
+			fixed
+			app
+		>
+			<v-list>
+				<v-list-item
+					v-for="(item, i) in items"
+					:key="i"
+					:to="item.to"
+					router
+					exact
+				>
+					<v-list-item-action>
+						<v-icon>{{ item.icon }}</v-icon>
+					</v-list-item-action>
+					<v-list-item-content>
+						<v-list-item-title v-text="item.title" />
+					</v-list-item-content>
+				</v-list-item>
+			</v-list>
+		</v-navigation-drawer>
+		<v-app-bar :clipped-left="clipped" fixed app>
+			<v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+			<v-btn icon @click.stop="miniVariant = !miniVariant">
+				<v-icon
+					>mdi-{{
+						`chevron-${miniVariant ? 'right' : 'left'}`
+					}}</v-icon
+				>
+			</v-btn>
+			<v-btn icon @click.stop="clipped = !clipped">
+				<v-icon>mdi-application</v-icon>
+			</v-btn>
+			<v-btn icon @click.stop="fixed = !fixed">
+				<v-icon>mdi-minus</v-icon>
+			</v-btn>
+			<v-toolbar-title v-text="title" />
+			<v-spacer />
+			<v-btn icon @click.stop="rightDrawer = !rightDrawer">
+				<v-icon>mdi-menu</v-icon>
+			</v-btn>
+		</v-app-bar>
+		<v-main>
+			<v-container>
+				<Nuxt />
+			</v-container>
+		</v-main>
+		<v-navigation-drawer
+			v-model="rightDrawer"
+			:right="right"
+			temporary
+			fixed
+		>
+			<v-list>
+				<v-list-item @click.native="right = !right">
+					<v-list-item-action>
+						<v-icon light> mdi-repeat </v-icon>
+					</v-list-item-action>
+					<v-list-item-title
+						>Switch drawer (click me)</v-list-item-title
+					>
+				</v-list-item>
+			</v-list>
+		</v-navigation-drawer>
+		<v-footer :absolute="!fixed" app>
+			<span>&copy; {{ new Date().getFullYear() }} MOBEIGEGE</span>
+		</v-footer>
+	</v-app>
 </template>
 
 <script>
 import particleBac from '@/static/js/particle-bac'
 export default {
-  name: 'DefaultLayout',
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-        {
-          icon: 'mdi-dog',
-          title: 'Lottie',
-          to: '/lottie',
-        },
-        {
-          icon: 'mdi-cat',
-          title: 'PointLoader',
-          to: '/pointLoader',
-        },
-        {
-          icon: 'mdi-fish',
-          title: 'CircleLoader',
-          to: '/circleLoader',
-        },
-        {
-          icon: 'mdi-music',
-          title: 'SvgDemo',
-          to: '/svgDemo',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'use nuxt+Vuetify',
-    }
-  },
-  mounted() {
-    this.initParticle()
-  },
-  methods: {
-    initParticle() {
-      particleBac('.container', {
-        line: false,
-        color: 'rgba(65 ,160, 255 , 1)',
-        count: 50,
-        rate: 0.5,
-        radius: 5,
-        bounce: true,
-        'z-index': -1,
-      })
-    },
-  },
+	name: 'DefaultLayout',
+	data() {
+		return {
+			clipped: false,
+			drawer: false,
+			fixed: false,
+			items: [
+				{
+					icon: 'mdi-apps',
+					title: 'Welcome',
+					to: '/',
+				},
+				{
+					icon: 'mdi-chart-bubble',
+					title: 'Inspire',
+					to: '/inspire',
+				},
+				{
+					icon: 'mdi-dog',
+					title: 'Lottie',
+					to: '/lottie',
+				},
+				{
+					icon: 'mdi-cat',
+					title: 'PointLoader',
+					to: '/pointLoader',
+				},
+				{
+					icon: 'mdi-fish',
+					title: 'CircleLoader',
+					to: '/circleLoader',
+				},
+				{
+					icon: 'mdi-music',
+					title: 'SvgDemo',
+					to: '/svgDemo',
+				},
+			],
+			miniVariant: false,
+			right: true,
+			rightDrawer: false,
+			title: 'use nuxt+Vuetify',
+		}
+	},
+	mounted() {
+		this.initParticle()
+	},
+	methods: {
+		initParticle() {
+			particleBac('.container', {
+				line: false,
+				color: 'rgba(65 ,160, 255 , 1)',
+				count: 50,
+				rate: 0.5,
+				radius: 5,
+				bounce: true,
+				'z-index': -1,
+			})
+		},
+	},
 }
 </script>
 <style lang="scss" scoped>
 .container {
-  position: relative;
-  height: 100%;
+	position: relative;
+	height: 100%;
+	.row {
+		position: relative;
+		z-index: 2;
+		height: 100%;
+	}
 }
 </style>
--- pages/circleLoader.vue
+++ pages/circleLoader.vue
@@ -1,34 +1,92 @@
 <template>
-  <v-row class="main-containers">
-    <v-col class="text-center">
-      <div class="donut"></div>
-    </v-col>
-  </v-row>
+	<v-row class="main-containers">
+		<v-col class="text-center">
+			<div class="donut"></div>
+			<div class="bubble-container">
+				<span v-for="i in 200" :key="i" class="bubble-item"></span>
+			</div>
+		</v-col>
+	</v-row>
 </template>
 <script>
 export default {
-  name: 'CircleLoader',
-  data() {
-    return {}
-  },
+	name: 'CircleLoader',
+	data() {
+		return {}
+	},
 }
 </script>
 <style lang="scss" scoped>
-.donut {
-  border: 4px solid rgba(0, 0, 0, 0.1);
-  border-top-color: #7983ff;
-  border-radius: 50%;
-  width: 50px;
-  height: 50px;
-  animation: donut-spin 1.2s linear infinite;
-  margin: 0 auto;
+$myColor:skyblue;
+.text-center {
+	position: relative;
+	min-height: 300px;
+	background: #fff;
+	filter: contrast(8);
+	.donut {
+		border: 4px solid rgba(0, 0, 0, 0.1);
+		border-top-color: #7983ff;
+		border-radius: 50%;
+		width: 50px;
+		height: 50px;
+		animation: donut-spin 1.2s linear infinite;
+		margin: 0 auto;
+	}
+	.bubble-container {
+		display: flex;
+		flex-wrap: nowrap;
+		gap: 5px;
+		position: absolute;
+		width: 100%;
+		height: 100px;
+		left: 0;
+		bottom: 0;
+    background: $myColor;
+		&:before {
+			content: '';
+			position: absolute;
+			top: -300px;
+			left: 0;
+			right: 0;
+			bottom: 0;
+			z-index: 1;
+			backdrop-filter: blur(5px);
+		}
+		@for $i from 1 to 11 {
+			.bubble-item:nth-child(#{$i}) {
+				border-radius: 50%;
+				position: absolute;
+				background-color: $myColor;
+				$width: random(100) + px;
+				left: #{(random(100)) + '%'};
+				top: #{(random(100))}px;
+				width: $width;
+				height: $width;
+				animation: moveToTop
+					#{(random(2500) + 1500) /
+					1000}s
+					ease-in-out -#{random(5000) /
+					1000}s
+					infinite;
+			}
+		}
+	}
+}
+@keyframes moveToTop {
+	90% {
+		opacity: 1;
+	}
+	100% {
+		opacity: 0.08;
+		transform: translate(-50%, -180px) scale(0.3);
+	}
 }
 @keyframes donut-spin {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
+	0% {
+		transform: rotate(0deg);
+	}
+	100% {
+		transform: rotate(360deg);
+	}
 }
 </style>
